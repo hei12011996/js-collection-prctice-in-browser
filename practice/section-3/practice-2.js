1375812de4db300116799e43e3b223a9c7a60032
Antony Chan <antony.chan@oocl.com>
2018-11-12 22:21:51 +0800

    Complete Section-3 Practice-2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,17 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var return_array = [];
+	collectionA.forEach(function(element){
+		if(objectB.value.includes(element.key)){
+			return_array.push({
+				key: element.key, count: element.count - parseInt(element.count / 3)
+			});
+		} else {
+			return_array.push(element);
+		}
+	})
+	
+  // return 'Implement the practice require, and begin changing code in this row';
+	return return_array;
 }
