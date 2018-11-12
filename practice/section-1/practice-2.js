5a3810c5e3add652c89f812e96265d8d5988202a
Antony Chan <antony.chan@oocl.com>
2018-11-12 15:28:37 +0800

    Complete Section-1 Practice-2

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var return_array = [];
+	collectionA.forEach(function(element){
+		collectionB.forEach(function(subCollection){
+			if(subCollection.includes(element)){
+				return_array.push(element);
+			}
+		})
+	})
+  // return 'Implement the practice require, and begin changing code in this row';
+	return return_array;
 }
