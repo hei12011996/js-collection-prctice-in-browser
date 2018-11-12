47b397fd79637c7cc1e56a84f5dc58a282ccffc0
Antony Chan <antony.chan@oocl.com>
2018-11-12 16:03:26 +0800

    Complete Section-2 Practice-2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,31 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var return_array = [];
+	collection.forEach(function(element){
+		var found = 0;
+		return_array.forEach(function(valueObject){
+			if(valueObject.key == element){
+				found = 1;
+				valueObject.count++;
+			}
+		})
+		if(found == 0){
+			if(element.indexOf('-') == -1){
+				return_array.push({
+					key: element,
+					count: 1
+				})
+			} else {
+				var breakOffValue = element.substring(element.indexOf('-') + 1);
+				return_array.push({
+					key: element.substring(0, element.indexOf('-')),
+					count: Number(breakOffValue)
+				})
+			}
+		}
+	})
+	
+  // return 'Implement the practice require, and begin changing code in this row';
+  return return_array;
 }
