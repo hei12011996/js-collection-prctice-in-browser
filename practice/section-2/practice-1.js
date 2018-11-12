04da3bde9b5e21fc207c8092b23c1fc2ff467b81
Antony Chan <antony.chan@oocl.com>
2018-11-12 16:02:58 +0800

    Complete Section-2 Practice-1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var return_array = [];
+	collection.forEach(function(element){
+		var found = 0;
+		return_array.forEach(function(valueObject){
+			if(valueObject.key == element){
+				found = 1;
+				valueObject.count++;
+			}
+		})
+		if(found == 0){
+			return_array.push({
+				key: element,
+				count: 1
+			})
+		}
+	})
+	
+  // return 'Implement the practice require, and begin changing code in this row';
+  return return_array;
 }
