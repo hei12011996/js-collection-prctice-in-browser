a63a41ecb8855ea19a05b072be602304d8a0e2ad
Antony Chan <antony.chan@oocl.com>
2018-11-21 11:42:36 +0800

    Reimplement section-1 practice-1 & practice-4 in non-loop way

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,12 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-	var return_array = [];
-	collectionA.forEach(function(element){
-		if(collectionB.includes(element)){
-			return_array.push(element);
-		}
-	});
-  //return 'Implement the practice require, and begin changing code in this row';
-	return return_array;
+	return collectionA.filter(element => collectionB.includes(element));
 }
--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,12 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-	var return_array = [];
-	collectionA.forEach(function(elementObject){
-		if(objectB.value.includes(elementObject.key)){
-			return_array.push(elementObject.key);
-		}
-	})
-  // return 'Implement the practice require, and begin changing code in this row';
-	return return_array;
+	return collectionA.filter(elementObject => objectB.value.includes(elementObject.key)).map(elementObject => elementObject.key);
 }
